470d7205a2ec50320fb5da79c44c17feeffe39e5
joao leao <jvgsfl@discente.ifpe.edu.br>
2021-11-22 17:23:03 -0300

    Firebase

--- screens/cadastro.js
+++ screens/cadastro.js
@@ -1,33 +1,62 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Image} from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function App({ navigation }) {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnDvrNKpMtOk07pjVAdwvL3uMKp3IXvvM",
+    authDomain: "my-app-3a79d.firebaseapp.com",
+    projectId: "my-app-3a79d",
+    storageBucket: "my-app-3a79d.appspot.com",
+    messagingSenderId: "941678975132",
+    appId: "1:941678975132:web:44076a051e95ce6fe7f559",
+    measurementId: "G-1M04HD4B9Y"
+  };
+
+  function cadastroContato(){
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+            // Signed in
+            console.log("cadastrado");
+            const user = userCredential.user;
+            navigation.navigate("Login");
+            // ...
+        })
+        .catch((error) => {
+          console.log("Não cadastrado");
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+
+  }
+
   return (
     <View>
 
         
-       <TextInput
-        style={styles.input}
-        placeholder="Nome"
-        keyboardType="Nome"
-      />
-        <TextInput
-        style={styles.input}
-        placeholder="cpf"
-        keyboardType="cpf"
-      />
         <TextInput
         style={styles.input}
         placeholder="email"
         keyboardType="email"
+        value = {email}
+        onChangeText ={email => setEmail(email)}
       />
       <TextInput
+        secureTextEntry={true}
         style={styles.input}
         placeholder="senha"
         keyboardType="senha"
+        value = {senha}
+        onChangeText ={senha => setSenha(senha)}
       />
-      <Button  color='#737373' title="salvar" onPress={() => navigation.navigate("Login")}>
+      <Button  color='#737373' title="salvar" onPress={() => {cadastroContato()}}>
 
       </Button>
     </View>
--- screens/login.js
+++ screens/login.js
@@ -1,9 +1,47 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { StyleSheet, Text, View, TextInput, Image} from 'react-native';
-import { Header, Button} from 'react-native-elements' 
+import { Header, Button} from 'react-native-elements';
 
 export default function login({ navigation }) {
+  
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnDvrNKpMtOk07pjVAdwvL3uMKp3IXvvM",
+    authDomain: "my-app-3a79d.firebaseapp.com",
+    projectId: "my-app-3a79d",
+    storageBucket: "my-app-3a79d.appspot.com",
+    messagingSenderId: "941678975132",
+    appId: "1:941678975132:web:44076a051e95ce6fe7f559",
+    measurementId: "G-1M04HD4B9Y"
+  };
+  
+  function loginContato(){
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+            // Signed in
+            console.log("Conectado");
+            const user = userCredential.user;
+            navigation.navigate("Home");
+            // ...
+        })
+        .catch((error) => {
+          console.log("Não conectado");
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+
+  }
+
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
   return (
     <View>
       
@@ -16,20 +54,27 @@ export default function login({ navigation }) {
         />
        <TextInput
         style={styles.input}
-        placeholder="Login"
-        keyboardType="Login"
+        placeholder="Email"
+        keyboardType="Email"
+        value = {email}
+        onChangeText ={email => setEmail(email)}
       />
         <TextInput
+        secureTextEntry={true}
         style={styles.input}
         placeholder="Senha"
         keyboardType="Senha"
+        value = {senha}
+        onChangeText ={senha => setSenha(senha)}
       />
-      <Button style={{padding:20}} color='#737373' title="Login" onPress={() => navigation.navigate("Home")}>
+      <Button style={{padding:20}} color='#737373' title="Login" onPress={() =>{loginContato()} }>
 
       </Button>
       <Button style={{padding:20}} color='#737373' title="cadastro"  onPress={() => navigation.navigate("Cadastro")}>
       </Button>
+
     </View>
+  
   );
 }
 
